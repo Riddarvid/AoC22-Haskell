c409e8487038626b10b93e949b286065f98e2bd0
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2022-12-01 13:52:43 +0100

    Solve Day1

--- src/day1/Day1.hs
+++ src/day1/Day1.hs
@@ -1,4 +1,32 @@
 module Day1 (solve) where
+import           Data.List (sort)
 
 solve :: String -> (Integer, Integer)
-solve = undefined
+solve input = (part1, part2)
+  where
+    invs = getInventories input
+    part1 = maxCalories 1 invs
+    part2 = maxCalories 3 invs
+
+-- Solution
+
+maxCalories :: Int -> [Inventory] -> Integer
+maxCalories n xs = sum $ take n $ reverse $ sort $ map sum xs
+
+-- Parsing
+
+type Inventory = [Integer]
+
+getInventories :: String -> [Inventory]
+getInventories input = map stringsToInts stringInvs
+  where
+    stringInvs = splitInventories $ lines input
+
+splitInventories :: [String] -> [[String]]
+splitInventories [] = []
+splitInventories xs = inv : splitInventories (drop 1 xs')
+  where
+    (inv, xs') = span (/= "") xs
+
+stringsToInts :: [String] -> [Integer]
+stringsToInts = map read

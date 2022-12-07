123a0e794b13f36498cbd9176f56270ecee5ad58
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2022-12-08 00:09:11 +0100

    Refactor parsing

--- src/day7/Day7.hs
+++ src/day7/Day7.hs
@@ -3,12 +3,6 @@ import           Data.HashMap.Lazy (HashMap, (!))
 import qualified Data.HashMap.Lazy as HM
 import           Solution          (Solution (I))
 
-data Instruction = ILs [FileDesc] | ICd String
-  deriving (Show)
-
-data FileDesc = FDir String | FFile String Integer
-  deriving (Show)
-
 type Dir = (HashMap String FileDir)
 
 data FileDir = Dir Dir | File Integer
@@ -17,58 +11,43 @@ data FileDir = Dir Dir | File Integer
 solve :: String -> (Solution, Solution)
 solve input = (I part1, I part2)
   where
-    inputInstrs = parseInstrs input
-    root = parseRoot inputInstrs
+    root = parseRoot $ lines input
     part1 = sumBelowLimit root 100000
     part2 = smallestAbove root $ neededSpace root
 
--- Parse instructions
-
-parseInstrs :: String -> [Instruction]
-parseInstrs input = parseInstrs' $ lines input
-
-parseInstrs' :: [String] -> [Instruction]
-parseInstrs' [] = []
-parseInstrs' (x:xs) = instr : parseInstrs' xs'
-  where
-    (instr, xs') = parseInstr x xs
-
-parseInstr :: String -> [String] -> (Instruction, [String])
-parseInstr line xs
-  | tokens !! 1 == "cd" = (ICd (tokens !! 2), xs)
-  | otherwise = parseLs xs
-  where
-    tokens = words line
-
-parseLs :: [String] -> (Instruction, [String])
-parseLs xs = (ILs (map parseFileDesc fileDirs), drop (length fileDirs) xs)
-  where
-    fileDirs = takeWhile (\s -> head s /= '$') xs
-
-parseFileDesc :: String -> FileDesc
-parseFileDesc line
-  | head tokens == "dir" = FDir (tokens !! 1)
-  | otherwise = FFile (tokens !! 1) (read (head tokens))
-  where
-    tokens = words line
-
 -- Parse file tree
 
-parseRoot :: [Instruction] -> Dir
+parseRoot :: [String] -> Dir
 parseRoot instrs = parseRoot' instrs [] HM.empty
 
-parseRoot' :: [Instruction] -> [String] -> Dir -> Dir
---parseRoot' instrs path dir | trace ("Remaining instrs: " ++ show instrs ++ "\nCurrent path: " ++ show path ++ "\nCurrent tree: " ++ show dir ++ "\n") False = undefined
+parseRoot' :: [String] -> [String] -> Dir -> Dir
 parseRoot' [] _ dir = dir
-parseRoot' (ICd dest:instrs) path dir = parseRoot' instrs path' dir
+parseRoot' (instr:instrs) path dir | tokens !! 1 == "cd" = parseRoot' instrs path' dir
   where
+    tokens = words instr
+    dest = tokens !! 2
     path' = case dest of
       "/"   -> []
       ".."  -> tail path
       dest' -> dest' : path
-parseRoot' (ILs files:instrs) path dir = parseRoot' instrs path dir'
+parseRoot' (instr:instrs) path dir | tokens !! 1 == "ls" = parseRoot' instrs' path dir'
   where
-    dir' = insertByPath path (newDir files) dir
+    tokens = words instr
+    (lsDir, instrs') = parseLs instrs
+    dir' = insertByPath path lsDir dir
+parseRoot' _ _ _ = error "Parse error"
+
+parseLs :: [String] -> (Dir, [String])
+parseLs xs = (HM.fromList (map parseFileDir fileDirs), drop (length fileDirs) xs)
+  where
+    fileDirs = takeWhile (\s -> head s /= '$') xs
+
+parseFileDir :: String -> (String, FileDir)
+parseFileDir line
+  | head tokens == "dir" = (tokens !! 1, Dir HM.empty)
+  | otherwise = (tokens !! 1, File $ read $ head tokens)
+  where
+    tokens = words line
 
 insertByPath :: [String] -> Dir -> Dir -> Dir
 insertByPath path = insertByPath' (reverse path)
@@ -80,13 +59,6 @@ insertByPath' (name:path) toInsert dir = HM.insert name (Dir updated) dir
     updated = case dir ! name of
       (File _)   -> error "Can't cd into file"
       (Dir dir') -> insertByPath' path toInsert dir'
-
-newDir :: [FileDesc] -> Dir
-newDir descs = HM.fromList $ map descToMap descs
-
-descToMap :: FileDesc -> (String, FileDir)
-descToMap (FDir name)       = (name, Dir HM.empty)
-descToMap (FFile name size) = (name, File size)
 
 -- Part 1
 

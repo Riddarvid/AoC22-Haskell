0e9a879cdcddf72dced7ecccfce57ab5a6b0ec96
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2022-12-13 12:37:03 +0100

    Add map helper

--- src/StringUtils.hs
+++ src/StringUtils.hs
@@ -1,9 +1,19 @@
-module StringUtils (getInts) where
+module StringUtils (getInts, stringsToCharMap) where
 
-import           Text.Regex.PCRE (AllTextMatches (getAllTextMatches), (=~))
+import           Data.HashMap.Lazy (HashMap)
+import qualified Data.HashMap.Lazy as HM
+import           Text.Regex.PCRE   (AllTextMatches (getAllTextMatches), (=~))
 
 intRegex :: String
 intRegex = "\\d+"
 
 getInts :: String -> [Integer]
 getInts input = map read $ getAllTextMatches $ input =~ intRegex
+
+type Pos = (Int, Int)
+
+stringsToCharMap :: [String] -> HashMap Pos Char
+stringsToCharMap input = HM.fromList mapList
+  where
+    charList = concat input
+    mapList = zip [(x, y) | y <- [0 .. length input - 1], x <- [0 .. length (head input) - 1]] charList
